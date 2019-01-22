182970848d56d4bfccfdc96d53829d7f2761f888
Patricio <patricioprp06@gmail.com>
2019-01-22 15:48:05 -0300

    se termina la relacion entre alumno y nota adicional

--- app/Alumno.php
+++ app/Alumno.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Alumno extends Model
 {
     protected $table = 'ALUMNOS';
+    protected $primaryKey = 'IDALUMNO';
     protected $fillable = ['IDALUMNO','APELLIDOS','NOMBRES','DOC','SEXO','GRPSANG','FECHA_NACIM','PROVNAC','PROVDOM','CODIGO_POSTAL','DOMICILIO','TELEFONO','EMAIL','PIN','COLSEC','PERS_USR','PERS_DAT','PERS_OBS','TELEFONO1','TELEFONO2','PROFESION','TRABAJO','NUMHIJOS','FECHA_FALLEC','NUMHERMANOS','REMUNERACION','TIPOVIVIENDA','VALORVIVIENDA','CUIL','IDPAIS','IDLOCALIDAD','IDLOCALIDADNAC','IDDOCTIPO','INGRESO','EGRESO','CODIGOMORA','FECHAMORA','CBU','ACTAMATRICULA','ESTADOCIVIL','FOTO','SALUD'];
+
+    public function notAdicionals(){
+        return $this->hasMany('\App\NotAdicional','IDNOTAADICIONAL');
+      }
 }
--- app/NotAdicional.php
+++ app/NotAdicional.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class NotAdicional extends Model
 {
     protected $table = 'NOTADICIONAL';
-    protected $fillable = ['IDNOTADICIONAL','ANO','IDALUMNO','IDPLANASIGADIC','NOTA','IDMODALIDAD','ORDEN','ASIGNATURAID'];
+    protected $primaryKey = 'IDNOTAADICIONAL';
+    protected $fillable = ['IDNOTAADICIONAL','ANO','IDALUMNO','IDPLANASIGADIC','NOTA','IDMODALIDAD','ORDEN','ASIGNATURAID'];
+
+    public function alumno(){
+        return $this->belongsTo('\App\Alumno','IDALUMNO');
+      }
 }
